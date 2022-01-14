764057a5aacbbc44813cd94f5c805e0aa46b54ca
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 19:31:01 +0200

    Create separated CL for request, responses and dtos

---
+++ src/Blog.Models/Requests/Category/AddCategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blog.Models.Requests.Category
+{
+    public class AddCategoryRequest
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Blog.Models/Responses/Category/AddCategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Blog.Models.Responses.Category
+{
+    public class AddCategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
