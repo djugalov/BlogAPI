86c2d15da23cb94b7ca5885d9c8edac0253fc78b
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 21:00:36 +0200

    Decorate props with JsonPropertyName attribute

--- src/Blog.Models/Responses/Category/DeleteCategoryResponse.cs
+++ src/Blog.Models/Responses/Category/DeleteCategoryResponse.cs
@@ -1,8 +1,13 @@
-﻿namespace Blog.Models.Responses.Category
+﻿using System.Text.Json.Serialization;
+
+namespace Blog.Models.Responses.Category
 {
     public class DeleteCategoryResponse
     {
+        [JsonPropertyName("isSuccess")]
         public bool IsSuccess { get; set; }
+
+        [JsonPropertyName("responseMessage")]
         public string DeleteCategoryResponseMessage { get; set; }
     }
 }
