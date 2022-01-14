867c1ab84398de48fea6c6f704a5a60881390631
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 21:01:10 +0200

    Add separate folder for DTOs

---
+++ src/Blog.Models/DTOs/Category/EditedCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models.DTOs.Category
+{
+    public class EditedCategoryDto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
