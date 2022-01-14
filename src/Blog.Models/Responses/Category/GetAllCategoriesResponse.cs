4c0747dbaeec94ba1bb378f539af916d43b6a6a9
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 00:31:54 +0200

    Add response classes for getting all categories and category by id

---
+++ src/Blog.Models/Responses/Category/GetAllCategoriesResponse.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Models.DTOs.Category;
+using System.Collections.Generic;
+
+namespace Blog.Models.Responses.Category
+{
+    public class GetAllCategoriesResponse
+    {
+        public IEnumerable<BaseCategoryDto> Categories { get; set; }
+    }
+}
---
+++ src/Blog.Models/Responses/Category/GetCategoryByIdResponse.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models.DTOs.Category;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models.Responses.Category
+{
+    public class GetCategoryByIdResponse
+    {
+        [JsonPropertyName("category")]
+        public BaseCategoryDto Category { get; set; }
+
+        [JsonPropertyName("isSuccess")]
+        public bool IsSuccess { get; set; }
+
+        [JsonPropertyName("responseMessage")]
+        public string GetCategoryByIdResponseMessage { get; set; }
+    }
+}
