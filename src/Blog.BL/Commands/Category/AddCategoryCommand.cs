0b79128e0e699c78edb8a16df5b1d098ea4bcb60
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 19:30:29 +0200

    Add AddCategoryCommand and AddCategoryCommandHandler

---
+++ src/Blog.BL/Commands/Category/AddCategoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models.Requests.Category;
+using Blog.Models.Responses.Category;
+using MediatR;
+
+namespace Blog.BL.Commands.Category
+{
+    public class AddCategoryCommand : IRequest<AddCategoryResponse>
+    {
+        public AddCategoryCommand(AddCategoryRequest request)
+        {
+            Category = request;
+        }
+        public AddCategoryRequest Category { get; private set; }
+    }
+}
---
+++ src/Blog.BL/Commands/Category/AddCategoryCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Data;
+using Blog.Models.Responses.Category;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blog.BL.Commands.Category
+{
+    public class AddCategoryCommandHandler : IRequestHandler<AddCategoryCommand, AddCategoryResponse>
+    {
+        private readonly BlogDbContext _context;
+        public AddCategoryCommandHandler(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AddCategoryResponse> Handle(AddCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _context.Categories.AddAsync(new Data.DbModels.Category
+            {
+                Name = request.Category.Name
+            });
+
+            await _context.SaveChangesAsync();
+
+            return new AddCategoryResponse
+            {
+                Id = category.Entity.Id,
+                Name = category.Entity.Name
+            };
+        }
+    }
+}
