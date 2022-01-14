fb871db283dd64580d3ef77c31c1acb3bedc88d2
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 19:29:43 +0200

    Add service collection extension for dependency injection

---
+++ src/Blog.BL/Services.DI/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Blog.BL.Commands.Category;
+using Blog.Models.Responses.Category;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddTransient<IRequestHandler<AddCategoryCommand, AddCategoryResponse>, AddCategoryCommandHandler>();
+
+            return services;
+        }
+    }
+}
