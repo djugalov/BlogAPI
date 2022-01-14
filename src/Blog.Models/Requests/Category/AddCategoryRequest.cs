f5d4a690acad48d2fdb01aa70b9b62ff74c1cad3
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-14 19:48:38 +0200

    Mark name as required

--- src/Blog.Models/Requests/Category/AddCategoryRequest.cs
+++ src/Blog.Models/Requests/Category/AddCategoryRequest.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Blog.Models.Requests.Category
 {
     public class AddCategoryRequest
     {
+        [Required(ErrorMessage = "Name must be specified")]
         [JsonPropertyName("name")]
         public string Name { get; set; }
     }
