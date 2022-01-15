d1237a37fd410fff335008f42652568bdd5c00ae
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 16:07:58 +0200

    Add Post details dto

---
+++ src/Blog.Models/DTOs/Post/PostDetailsDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models.DTOs.Post
+{
+    public class PostDetailsDto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("authorId")]
+        public Guid AuthorId { get; set; }
+
+        [JsonPropertyName("publishedOn")]
+        public DateTime PublishedOn { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
