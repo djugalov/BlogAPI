eaa68943f2eb5c85da7ee2e973c7963b11536a05
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:10:43 +0200

    Add request class for post creation

---
+++ src/Blog.Models/Requests/Post/CreatePostRequest.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models.DTOs.Post;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models.Requests.Post
+{
+    public class CreatePostRequest
+    {
+        public CreatePostRequest(CreatePostDto post)
+        {
+            Post = post;
+        }
+
+        [Required(ErrorMessage = "Information about new post must be provided")]
+        public CreatePostDto Post { get; set; }
+    }
+}
