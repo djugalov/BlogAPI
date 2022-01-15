cf981fd920aa19ac539394065d94578666f8cbc9
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:10:17 +0200

    Add response class for post creation

---
+++ src/Blog.Models/Responses/Post/CreatePostResponse.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Models.DTOs.Post;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models.Responses.Post
+{
+    public class CreatePostResponse
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("post")]
+        public CreatePostDto Post { get; set; }
+
+        [JsonPropertyName("isSuccess")]
+        public bool IsSuccess { get; set; }
+
+        [JsonPropertyName("responseMessage")]
+        public string ResponseMessage { get; set; }
+    }
+}
