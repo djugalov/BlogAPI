d1658b2c869b6cc95dec09777eee7e922c23a885
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:31:14 +0200

    Removed unused usings

--- src/Blog.BL/Commands/Category/EditCategoryCommandHandler.cs
+++ src/Blog.BL/Commands/Category/EditCategoryCommandHandler.cs
@@ -1,10 +1,6 @@
 ﻿using Blog.Data;
 using Blog.Models.Responses.Category;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
