049a38529f66bac1060010ebef6efe13f97e3cf2
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:11:14 +0200

    Add createPost Dto

---
+++ src/Blog.Models/DTOs/Post/CreatePostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models.DTOs.Post
+{
+    public class CreatePostDto
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("authorId")]
+        public Guid AuthorId { get; set; }
+
+    }
+}
