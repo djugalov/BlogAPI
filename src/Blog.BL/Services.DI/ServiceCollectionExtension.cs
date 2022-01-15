c91242d710c94c3d214a42923260618981dfe9ea
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:13:10 +0200

    Register new services

--- src/Blog.BL/Services.DI/ServiceCollectionExtension.cs
+++ src/Blog.BL/Services.DI/ServiceCollectionExtension.cs
@@ -1,6 +1,8 @@
 ﻿using Blog.BL.Commands.Category;
+using Blog.BL.Commands.Post;
 using Blog.BL.Queries.Category;
 using Blog.Models.Responses.Category;
+using Blog.Models.Responses.Post;
 using MediatR;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -9,13 +11,25 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddProjectServices(this IServiceCollection services)
         {
+            AddCategoryServices(services);
+
+            AddPostServices(services);
+
+            return services;
+        }
+
+        private static void AddCategoryServices(IServiceCollection services)
+        {
             services.AddTransient<IRequestHandler<AddCategoryCommand, AddCategoryResponse>, AddCategoryCommandHandler>();
             services.AddTransient<IRequestHandler<DeleteCategoryCommand, DeleteCategoryResponse>, DeleteCategoryCommandHandler>();
             services.AddTransient<IRequestHandler<EditCategoryCommand, EditCategoryResponse>, EditCategoryCommandHandler>();
             services.AddTransient<IRequestHandler<GetCategoryByIdQuery, GetCategoryByIdResponse>, GetCategoryByIdQueryHandler>();
             services.AddTransient<IRequestHandler<GetAllCategoriesQuery, GetAllCategoriesResponse>, GetAllCategoriesQueryHandler>();
+        }
 
-            return services;
+        private static void AddPostServices(IServiceCollection services)
+        {
+            services.AddTransient<IRequestHandler<CreatePostCommand, CreatePostResponse>, CreatePostCommandHandler>();
         }
     }
 }
