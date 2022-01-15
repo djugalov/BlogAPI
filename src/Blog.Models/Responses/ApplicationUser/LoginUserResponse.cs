3cc080912c81a94d308a862c19547aa88fe0fb49
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 23:27:48 +0200

    Add login request and response

---
+++ src/Blog.Models/Responses/ApplicationUser/LoginUserResponse.cs
@@ -0,0 +1,9 @@
+﻿using Blog.Models.Abstractions;
+
+namespace Blog.Models.Responses.ApplicationUser
+{
+    public class LoginUserResponse : BaseResponse
+    {
+        public string Token { get; set; }
+    }
+}
