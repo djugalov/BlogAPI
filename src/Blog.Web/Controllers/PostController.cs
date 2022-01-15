7cd67aae7687002d9812afbc181e6587b0f1f562
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-15 12:12:11 +0200

    Create endpoint for post creation

---
+++ src/Blog.Web/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Blog.BL.Commands.Post;
+using Blog.Models.Requests.Post;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public PostController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost([FromBody] CreatePostRequest request)
+        {
+            var response = await _mediator.Send(new CreatePostCommand(request));
+
+            return Ok(response);
+        }
+    }
+}
