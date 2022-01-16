8569ee10908b01b47efd5848c176ce4244140aa8
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-16 16:07:36 +0200

    Make tag name unique

--- src/Blog.Data/DbModels/Tag.cs
+++ src/Blog.Data/DbModels/Tag.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Data.DbModels
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Tag
     {
         [Key]
