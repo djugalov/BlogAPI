781aec07c9cfaad649fe7987693adaa8ed370ec3
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-16 16:17:00 +0200

    Build skeleton for of tag controller with respective endpoint

---
+++ src/Blog.Web/Controllers/TagController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TagController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TagController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTags()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTag()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyTagsToPost()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveTagFromPost()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteTag()
+        {
+            return Ok();
+        }
+    }
+}
