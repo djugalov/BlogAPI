ff0b6f5d2b084c473f67f7fc2540312602dc9319
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2022-01-16 12:53:02 +0200

    Add FK constraint to Post

---
+++ src/Blog.Data/Migrations/20220116011721_AddForeignKeyFromCommentToPost.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class AddForeignKeyFromCommentToPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "PostId",
+                table: "Comments",
+                newName: "post_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                newName: "IX_Comments_post_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_post_id",
+                table: "Comments",
+                column: "post_id",
+                principalTable: "Posts",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_post_id",
+                table: "Comments");
+
+            migrationBuilder.RenameColumn(
+                name: "post_id",
+                table: "Comments",
+                newName: "PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_post_id",
+                table: "Comments",
+                newName: "IX_Comments_PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
